84c52690ad36dedf608f253055981e51663baec5
oliver <13484957672@163.com>
2023-07-09 21:06:40 +0800

    static

--- meiduo_mail/meiduo_mail/settings.py
+++ meiduo_mail/meiduo_mail/settings.py
@@ -167,6 +167,11 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+# 告知系统去哪里 查找静态文件
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR,'static'),
+]
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
