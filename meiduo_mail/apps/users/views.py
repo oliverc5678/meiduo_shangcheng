0aea9ea45f2851b14bb6388d8bea432914e51f1c
oliver <13484957672@163.com>
2023-07-18 14:07:40 +0800

    状态保持

--- meiduo_mail/apps/users/views.py
+++ meiduo_mail/apps/users/views.py
@@ -23,6 +23,8 @@ logger = logging.getLogger('django')
     1.在函数(方法)的入口处
     2.在需要验证的地方添加
 """
+
+
 class RegisterView(View):
     """
     1.用户名我们需要判断是否重复(这个要开发一个视图)
@@ -44,12 +46,12 @@ class RegisterView(View):
         3.短信
         4.注册功能
     """
-    def get(self,request):
 
-        return render(request,'register.html')
+    def get(self, request):
 
+        return render(request, 'register.html')
 
-    def post(self,request):
+    def post(self, request):
         """
         1.接收前端提交的用户名,密码和手机号
         2.数据的验证（不相信前端提交的任何数据）
@@ -70,7 +72,7 @@ class RegisterView(View):
         # 2.数据的验证（不相信前端提交的任何数据）
         #     2.1 验证必传的数据（前端传给后端）是否有值
         #     all([el,el,el])el必须有值，只要有一个为None，则为Falese
-        if not all([username,password,password2,mobile]):
+        if not all([username, password, password2, mobile]):
             return HttpResponseBadRequest('参数有问题')
             # 2.2 判断用户名是否符合规则，判断5-20位数字 字母 _组合
         if not re.match(r'^[a-zA-Z0-9_]{5,20}$', username):
@@ -88,11 +90,19 @@ class RegisterView(View):
         # 3.入库-----验证数据没问题才入库
         # User.objects.create 直接入库，理论是没问题的，但是，密码是明文
         try:
-            user = User.objects.create_user(username=username,password=password,mobile=mobile)
+            user = User.objects.create_user(username=username, password=password, mobile=mobile)
         except Exception as e:
             logger.error(e)
-            return render(request,'register.html',context={'error_message':'数据库异常'})
+            return render(request, 'register.html', context={'error_message': '数据库异常'})
             return http.HttpResponseBadRequest('数据库异常')
         # 4.返回响应，跳转到首页
+        # 注册完成之后,默认认为用户已经登陆了
+        # 保持登陆的状态
+        # session
+        # 自己实现request.session
+
+        # 系统也能自己去帮助我们实现 登陆状态的保持
+        from django.contrib.auth import login
+        login(request, user)
         # return HttpResponse('注册成功')
         return redirect(reverse('contents:index'))
